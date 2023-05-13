3c0bc336042cdb83d3eca36b38b954864c2ef302
Anatoliy Tupitsin <hexlet@wailbox.ru>
2023-05-13 20:43:13 +0300

    done

--- index.js
+++ index.js
@@ -5,3 +5,24 @@ const array = [[12,3,4],[0,0,0],[23,5,6],[123,'6',-5],[0,'0','O',1],['test']]
 // если вы встречаете цифру в виде строки, ее нужно нормализовать и посчитать тоже
  
 // выведите итоговый массив в консоль
+
+const strNormalizeToZero = (num) => {
+  if(typeof num === "string"){
+    if(Number.isNaN(Number(num))){
+      return 0;
+    }
+    return Number(num);
+  }
+  return num;
+} 
+
+const sumArray = (array) => {
+  const result = array.map((arr) => {
+      const sum = arr.reduce((acc, num) => {
+        return (acc += strNormalizeToZero(num));
+      }, 0);
+      return sum;
+    });
+  return result;
+};
+console.log(sumArray(array));
